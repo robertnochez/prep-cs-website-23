68e0d7d1bfa77f0320e181aaa93f1387a7cd3cc9
Jesus Villalobos <jvillalobos@berkeley.edu>
2021-07-21 17:09:27 -0700

    Made changes to slide component appearance.

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,11 +1,20 @@
-import React from "react"
+import React from "react";
+import "../App.css";
 
 function Slide(props) {
   return (
-    <div class="responsive-google-slides">
-        <iframe src={`https://docs.google.com/presentation/d/e/2PACX-1vQ8FUk326z79IIS2bnHXF4tXOWXZGujzJ3BivfX3dfjH5JFpjQtigMdgtfRAl0y_-oPeeEb7eNFvGDS/embed?start=false&loop=false&delayms=3000`} frameborder="0" width="480" height="299" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    <div class="slides" style={{ textAlign: "center" }}>
+      <iframe
+        src={`https://docs.google.com/presentation/d/e/2PACX-1vQ8FUk326z79IIS2bnHXF4tXOWXZGujzJ3BivfX3dfjH5JFpjQtigMdgtfRAl0y_-oPeeEb7eNFvGDS/embed?start=false&loop=false&delayms=3000`}
+        frameborder="0"
+        // width="960"
+        // height="569"
+        allowfullscreen="true"
+        mozallowfullscreen="true"
+        webkitallowfullscreen="true"
+      ></iframe>
     </div>
-    )
+  );
 }
 
-export default Slide
+export default Slide;
