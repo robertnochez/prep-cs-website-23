52dc5b6eeea8c67ef5a22205db7dd4e07ecef6da
Jesus Villalobos <jvillalobos@berkeley.edu>
2021-07-25 20:15:39 -0700

    Testing repo rename.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -62,25 +62,9 @@ class Carousel extends React.Component {
         <Header as={"h3"}>
           <b>Sample Announcement</b>
         </Header>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <p>Second Announcement - Line 1</p>
         <br />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <p>Second Announcement - Line 2</p>
       </div>,
     ];
 
