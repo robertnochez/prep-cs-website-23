b6eef1893cd9c637a939f046946660e371549fc8
Jesus Villalobos <jvillalobos@berkeley.edu>
2021-07-27 18:34:40 -0700

    Corrected video source, and re-releasing WMCode Lesson 1 after issues deploying.

--- src/pages/PREP/PREPLesson2.js
+++ src/pages/PREP/PREPLesson2.js
@@ -3,19 +3,20 @@ import React from "react";
 import PREPLesson from "./Lesson";
 
 function PREPLesson2() {
-    return (
-      <PREPLesson 
-        header="Python Syntax"
-        description="An introduction to Python, including data types and its basic syntax. Definitions for 
+  return (
+    <PREPLesson
+      header="Python Syntax"
+      description="An introduction to Python, including data types and its basic syntax. Definitions for 
         different python operators and how to evaluate expressions using orders of operations, analogies, and examples. Keep 
         in mind that the actual language we're using doesn't matter too much, since what we're trying to teach here are features
         that are common to every programming language. This is a skill that will be useful for both CS10 and CS61A. :)"
-        slideSrc={`https://docs.google.com/presentation/d/e/2PACX-1vT4buFB6NV7iMWVAY4IziZE6ZKLoZEOATn6Hmusg0x8Mq2AmeLo0R4oB-itT2deqESoRmXCSHh3MZHa/embed?start=false&loop=false&delayms=3000`}
-        prevLesson=""
-        nextLesson=""
-        released={true}
-      />
-    )
+      slideSrc={`https://docs.google.com/presentation/d/e/2PACX-1vT4buFB6NV7iMWVAY4IziZE6ZKLoZEOATn6Hmusg0x8Mq2AmeLo0R4oB-itT2deqESoRmXCSHh3MZHa/embed?start=false&loop=false&delayms=3000`}
+      prevLesson=""
+      nextLesson=""
+      released={true}
+      videoSrc="https://www.youtube.com/embed/j1iVh5eHGxI"
+    />
+  );
 }
 
 export default PREPLesson2;
