8d4c2dd3d46f74518323e917e1d6289fac182e86
Jesus Villalobos <jvillalobos@berkeley.edu>
2021-08-01 22:59:53 -0700

    Added slides and folder download to PREP Lesson 6.

--- src/pages/PREP/PREPLesson6.js
+++ src/pages/PREP/PREPLesson6.js
@@ -8,9 +8,10 @@ function PREPLesson6() {
           header="Iterables and the 'For' Loop"
           description="What are iterables? What are the different types of iterables? Today we will discuss for loops and when
           to use them instead of while loops."
-          slideSrc=""
+          slideSrc="https://docs.google.com/presentation/d/e/2PACX-1vRjODe7bzL9NYyw9p-EyNZJ6lFu4CV0gzFQS0vjxICy3aeVsvqIc3BsahWS3frP9-SPm_A5qMt_SARL/embed?start=false&loop=false&delayms=3000" 
           prevLesson={null}
           nextLesson=""
+          lessonDownload="./prepDownloads/prep_lesson6.zip"
         />
     )
 }
