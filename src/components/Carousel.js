e3eff6129b544d68a6934a491c4b80e5a9cc9677
Jesus Villalobos <jvillalobos@berkeley.edu>
2021-08-03 19:44:44 -0700

    Released PREP Lesson 8; updated carousel to w slide on practice problems; updated slides link to open in a new page.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,46 +1,41 @@
 import React, { Component } from "react";
 import "../App.css";
 import { Grid, Header, Menu, Segment } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import { C } from "keyboard-key";
 
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeItem: "2",
+      activeItem: "3",
       activePage: [
         <div>
-        <Header as={"h3"}>
-          <b>Office Hours</b>
-        </Header>
-        <p>
-          As you may have already seen in your emails, we will be holding office hours 
-          throughout the week to make sure you have a space to ask any questions you have,
-          and have them answered. Current dates and times are listed below, and we may add 
-          more times if we see office hours are popular! Drop by to ask anything ... 
-          CS-related, Cal-related, or anything else!
-        </p>
-        <p>
-          <b>Karen's OH:</b> Mondays and Wednesdays, 5-6pm. 
-          <a
-            target="_blank" 
-            rel="noopener noreferrer"
-            href="https://berkeley.zoom.us/j/92460458306?pwd=RVlJN0lLU29JNzcySzFVTzFNd1BNdz09"
-          >
-            JOIN MEETING
-          </a>
-        </p>
-        <p>
-          <b>Cristian's OH:</b> Tuesdays and Thursdays, 5-6pm. 
-          <a
-            target="_blank" 
-            rel="noopener noreferrer"
-            href="https://berkeley.zoom.us/j/99662937200"
-          >
-            JOIN MEETING
-          </a>
-        </p>
-      </div>,
+          <Header as={"h3"}>
+            <b>Practice Problem Solutions</b>
+          </Header>
+          <p>
+            Moving forward, solutions to practice problems will be posted on
+            their respective page the night of the lesson. For example, the
+            solutions to Lesson 7 practice problems are posted the evening of
+            August 2nd.
+          </p>
+          <p>
+            While we may get to cover some problems during the lessons, we
+            cannot guarantee that we will always have time. So, if you have
+            questions or would like to work out the problems with your peers or
+            the instructors, please drop by Office Hours. We are more than happy
+            to help you figure out the problems and answer any questions you may
+            have :)
+          </p>
+          <p>You can find the links to OH on the slide 2 of this section!!</p>
+          <p>
+            <Link to="/prepcs-lesson6">Lesson 6 Solutions</Link>
+          </p>
+          <p>
+            <Link to="/prepcs-lesson7">Lesson 7 Solutions</Link>
+          </p>
+        </div>,
       ],
     };
   }
@@ -79,21 +74,60 @@ class Carousel extends React.Component {
           <b>Office Hours</b>
         </Header>
         <p>
-          As you may have already seen in your emails, we will be holding office hours 
-          throughout the week to make sure you have a space to ask any questions you have,
-          and have them answered. Current dates and times are listed below, and we may add 
-          more times if we see office hours are popular! Drop by to ask anything ... 
-          CS-related, Cal-related, or anything else!
+          As you may have already seen in your emails, we will be holding office
+          hours throughout the week to make sure you have a space to ask any
+          questions you have, and have them answered. Current dates and times
+          are listed below, and we may add more times if we see office hours are
+          popular! Drop by to ask anything ... CS-related, Cal-related, or
+          anything else!
         </p>
         <p>
-          <b>Karen's OH:</b> Mondays and Wednesdays, 5-6pm. 
+          <b>Karen's OH:</b> Mondays and Wednesdays, 5-6pm.
           <a
-            target="_blank" 
+            target="_blank"
             rel="noopener noreferrer"
             href="https://berkeley.zoom.us/j/92460458306?pwd=RVlJN0lLU29JNzcySzFVTzFNd1BNdz09"
           >
             JOIN MEETING
           </a>
+        </p>
+        <p>
+          <b>Cristian's OH:</b> Tuesdays and Thursdays, 5-6pm.
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://berkeley.zoom.us/j/99662937200"
+          >
+            JOIN MEETING
+          </a>
+        </p>
+      </div>,
+    ];
+
+    const page3 = [
+      <div>
+        <Header as={"h3"}>
+          <b>Practice Problem Solutions</b>
+        </Header>
+        <p>
+          Moving forward, solutions to practice problems will be posted on their
+          respective page the night of the lesson. For example, the solutions to
+          Lesson 7 practice problems are posted the evening of August 2nd.
+        </p>
+        <p>
+          While we may get to cover some problems during the lessons, we cannot
+          guarantee that we will always have time. So, if you have questions or
+          would like to work out the problems with your peers or the
+          instructors, please drop by Office Hours. We are more than happy to
+          help you figure out the problems and answer any questions you may have
+          :)
+        </p>
+        <p>You can find the links to OH on the slide 2 of this section!!</p>
+        <p>
+          <Link to="/prepcs-lesson6">Lesson 6 Solutions</Link>
+        </p>
+        <p>
+          <Link to="/prepcs-lesson7">Lesson 7 Solutions</Link>
         </p>
       </div>,
     ];
@@ -125,8 +159,9 @@ class Carousel extends React.Component {
                     onClick={this.handleItemClick}
                   />
                   <Menu.Item
-                    disabled
+                    disabled={false}
                     name="3"
+                    page={page3}
                     active={activeItem === "3"}
                     onClick={this.handleItemClick}
                   />
