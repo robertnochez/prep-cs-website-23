2d0a420713d108f4c286473ef355314ea3480f0d
Jesus Villalobos <jvillalobos@berkeley.edu>
2021-08-06 10:59:45 -0700

    Added solutions to practice problems.

--- src/pages/PREP/PREPLesson9.js
+++ src/pages/PREP/PREPLesson9.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 import PREPLesson from "./Lesson";
+import CodeBlock from "../../components/CodeBlock";
+import { Header } from "semantic-ui-react";
+
 
 function PREPLesson9() {
   return (
@@ -16,7 +19,82 @@ function PREPLesson9() {
       slideLink="https://docs.google.com/presentation/d/1WvbeP9aJ__6h1V7jSOXIMbfclHczygiEMflrcXmqJSs/edit?usp=sharing"
       lessonDownload="./prepDownloads/prep_lesson9.zip"
       videoSrc={`https://www.youtube.com/embed/hFAgvMiIkHI`}
-    />
+    >
+
+      <Header as={"h3"}>Practice Problem Solutions:</Header>
+
+      <CodeBlock
+        code={`
+"""
+NOTE: THERE WERE ERRORS IN THE DOCTESTS. IF YOUR CODE IS OUTPUTTING THE RIGHT THING
+THEN IT IS MOST LIKELY RIGHT. CHECK DOWN BELOW FOR THE ERRORS. THERE ARE COMMENTS
+IN THE DOCSTRINGS WHERE THE ERRORS WERE.
+"""
+
+
+fn = lambda x: lambda y: pow(x, y)
+
+def map_recursive(start, end, f):
+    """Implement map_to_range from Tuesday's lesson recursively
+    
+    >>> map_recursive(0, 10, fn(2))         ## there was accidentally two 128's in doctest
+    1
+    2
+    4
+    8
+    16
+    32
+    64
+    128        
+    256
+    512
+    """
+    if start == end:
+        return
+    print(f(start))
+    return map_recursive(start + 1, end, f)
+
+
+def fib(i):
+    """Return the i-th term of the Fibonacci Sequence.
+    
+    >>> fib(2)    # --> 0 + 1
+    1
+    >>> fib(5)    # --> 2 + 3
+    5
+    >>> fib(10)   # --> 21 + 34
+    55
+    """
+    if i == 0:
+        return 0
+    elif i == 1:
+        return 1
+    else:
+        return fib(i - 1) + fib(i - 2)
+
+
+def factorial_hof(n):
+    """Now that we have defined factorial using iteration and recursion,
+    implement it using a higher order function. You are not allowed to use
+    operators outside of the function calls. 
+    --> Think about what we can do to store a counter
+
+    >>> factorial_hof(5)
+    120
+    >>> factorial_hof(6) ## error in this doctect
+    720
+    """
+    def helper(x, y):
+        if x == 0:
+            return y
+        else:
+            return helper(x - 1, y * x)
+    return helper(n, 1)
+`} 
+      />
+
+    
+    </PREPLesson>
   );
 }
 
