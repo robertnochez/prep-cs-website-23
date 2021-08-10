0e6070a28b5b3ebc9d27fff329b129fa65eecd39
Jesus Villalobos <jvillalobos@berkeley.edu>
2021-08-10 03:14:55 -0700

    Released Lesson 13.

--- src/pages/PREP/PREPLesson12.js
+++ src/pages/PREP/PREPLesson12.js
@@ -11,6 +11,7 @@ function PREPLesson12() {
           slideSrc={`https://docs.google.com/presentation/d/e/2PACX-1vQWaia1luX6xwuNUsII5fas2NdwR6SE4TgiiU5luCpMBQPHw5SRhJ2yVTi_Opu5iZkbmc16PKt5azX2/embed?start=false&loop=false&delayms=3000`}
           prevLesson={null}
           nextLesson=""
+          slideSrc="https://docs.google.com/presentation/d/1c7hLPkfoiuhXeJeaTAF5VgzkQXh1kE_MHZVnjZ0_UHE/edit?usp=sharing"
         />
     )
 }
