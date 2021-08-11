84a08bb0146bb0f76e47fd24afe354ae8e6a68a3
Cristian Moran <cris.moran@berkeley.edu>
2021-08-11 11:09:44 -0700

    Add slide link for lesson 13

--- src/pages/PREP/PREPLesson13.js
+++ src/pages/PREP/PREPLesson13.js
@@ -10,6 +10,7 @@ function PREPLesson13() {
           slideSrc={`https://docs.google.com/presentation/d/e/2PACX-1vTFA1vZi8v-BUDXx-0-d8g1WV-25pOGiTi1LnwsSWZ7hP0yLluguMdxvUk1ProiIltcD39zXIwURZMA/embed?start=false&loop=false&delayms=3000`}
           prevLesson={null}
           nextLesson=""
+          slideLink="https://docs.google.com/presentation/d/1a0RXKnM9Xq7PXPThIQ3cQjmiQ--SX5s0QgV9JzP0Y84/edit?usp=sharing"
         />
     )
 }
