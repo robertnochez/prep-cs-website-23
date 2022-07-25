e6de78f6beccd757679723f34821f707e445b8df
Dest UL <destul@Destinys-MacBook-Pro.local>
2022-07-25 10:05:35 -0700

    Moved Vinc Code

--- src/pages/PREP/PREPLesson2.js
+++ src/pages/PREP/PREPLesson2.js
@@ -9,7 +9,7 @@ function PREPLesson2() {
       description="An introduction to Python, including data types and its basic syntax. Definitions for 
       different python operators and how to evaluate expressions using orders of operations, analogies, and examples. Keep 
       in mind that the actual language we're using doesn't matter too much, since what we're trying to teach here are features
-      that are common to every programming language. This is a skill that will be useful for both CS10 and CS61A. :)"
+      that are common to every programming language. This is a skill that will be useful for both CS10 and CS61A. :o"
       slideSrc={`https://docs.google.com/presentation/d/e/2PACX-1vT4buFB6NV7iMWVAY4IziZE6ZKLoZEOATn6Hmusg0x8Mq2AmeLo0R4oB-itT2deqESoRmXCSHh3MZHa/embed?start=false&loop=false&delayms=3000`}
       prevLesson=""
       nextLesson=""
