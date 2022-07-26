1de6a77e916a3c4ae26b6f020218750e46429629
Dest UL <destul@Destinys-MacBook-Pro.local>
2022-07-25 23:38:44 -0700

    Changed Bios

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,10 +6,10 @@ import ContactImage from "../images/Contact.png";
 import QuoteBlock from "../components/QuoteBlock";
 import Footer from "../components/Footer";
 
-import JesusBio from "../images/JesusBio.PNG";
-import CrisBio from "../images/CrisBio.jpg";
-import KarenBio from "../images/KarenBio.png";
-import YoussefBio from "../images/YoussefBio.png";
+import DestinyBio from "../images/DestinyBio.jpg";
+import VincentBio from "../images/VincentBio.jpg";
+import MarcosBio from "../images/MarcosBio.jpg";
+import LailaBio from "../images/SampleLesson.png";
 
 import { Card, Icon, Image, Grid, Header } from "semantic-ui-react";
 import TitleAndImageBlock from "../components/TitleAndImageBlock";
@@ -47,7 +47,7 @@ function About() {
               </Header>
               <p>
                 We are a group of undergraduate agents of change. Together,
-                Destiny, Marcos and Vincent developed curriculum to make CS
+                Destiny, Marcos, Laila and Vincent developed curriculum to make CS
                 Central a valuable resource for minority students to easily
                 transition into their post-secondary computer science careers.
               </p>
@@ -65,29 +65,21 @@ function About() {
         <Grid.Row columns={2} centered>
           <Grid.Column width={5}>
             <Card centered fluid>
-              <Image src={JesusBio} wrapped ui={false} />
+              <Image src={VincentBio} wrapped ui={false} />
               <Card.Content>
-                <Card.Header>Jesus Villalobos</Card.Header>
+                <Card.Header>Vincent Hong Lee</Card.Header>
                 <Card.Meta>
-                  CS Central Founder, Co-Site Admin, WMCode Curriculum Writer,
+                  CS Central 22 Co-Site Admin,
                   PREP CS Curriculum Co-Writer
                 </Card.Meta>
                 <Card.Description>
-                  Jesus is a second-year Electrical Engineering and Computer
-                  Sciences student at UC Berkeley. He is firm believer in equity
-                  and passionate about STEM education, and combined both by
-                  founding CS Central. At Berkeley, Jesus is a research
-                  assistant in the Algorithms and Computing Education (ACE) lab
-                  where he develops software for university courses, and is a
-                  mentor for first-year minority students. In the future, Jesus
-                  wants to expand CS Central to reach as many underrepresented
-                  students as it can, to open the doors to their futures in STEM
-                  and computer science.
+                 Hi everyone! I'm a 4th year EECS major, with interests in computer systems and security. 
+                 Some of my interests include music and badminton.
                 </Card.Description>
               </Card.Content>
               <Card.Content extra>
                 <Icon name="mail outline" bordered />
-                jvillalobos [at] berkeley [dot] edu
+                vlee747 [at] berkeley [dot] edu
               </Card.Content>
             </Card>
           </Grid.Column>
@@ -96,27 +88,21 @@ function About() {
 
           <Grid.Column width={5}>
             <Card centered fluid>
-              <Image src={CrisBio} wrapped ui={false} />
+              <Image src={DestinyBio} wrapped ui={false} />
               <Card.Content>
-                <Card.Header>Cristian Moran</Card.Header>
+                <Card.Header>Destiny Luong</Card.Header>
                 <Card.Meta>
-                  Co-Site Admin, PREP CS Curriculum Co-Writer
+                CS Central 22 Co-Site Admin, PREP CS Curriculum Co-Writer
                 </Card.Meta>
                 <Card.Description>
-                  Cristian is second year student at UC Berkeley majoring in
-                  Electrical Engineering and Computer Sciences. He is currently
-                  working in the Algorithms and Computing Education (ACE) lab,
-                  contributing to PrairieLearn, an open source learning platmorm
-                  used by education centers across the country. Within the
-                  Berkeley campus, Cris enjoys helping future generations and
-                  serves a mentor to first-year EECS students, along with being
-                  a tutor for the specialized CS Scholars section of the
-                  introductory programming course at Berkeley.
+                 Hey everyone! I'm Destiny, a rising junior interested in combining design and CS.
+                 I like drawing, cooking, and reading comics. Looking forward to meeting everyone
+                 this semester!
                 </Card.Description>
               </Card.Content>
               <Card.Content extra>
                 <Icon name="mail outline" bordered />
-                cris [dot] moran [at] berkeley [dot] edu
+                duluong [at] berkeley [dot] edu
               </Card.Content>
             </Card>
           </Grid.Column>
@@ -125,28 +111,20 @@ function About() {
         <Grid.Row columns={2} centered>
           <Grid.Column width={5}>
             <Card centered fluid>
-              <Image src={KarenBio} wrapped ui={false} />
+              <Image src={MarcosBio} wrapped ui={false} />
               <Card.Content>
-                <Card.Header>Karen Ruiz-Roca</Card.Header>
+                <Card.Header>Marcos Hernandez</Card.Header>
                 <Card.Meta>PREP CS Curriculum Co-Writer</Card.Meta>
                 <Card.Description>
-                  Karen is a second-year bioengineering student at UC Berkeley
-                  and a graduate of King-Drew Magnet Medical High School in Los
-                  Angeles, CA. At UC Berkeley, Karen is a summer undergraduate
-                  researcher in the Herr Lab. Her current focus is on the use of
-                  gel electrophoresis in clinical trials. She was part of the
-                  PREP program her freshman year and is currently a PREP
-                  Computer Science instructor for the Introduction to Python
-                  course made for first-generation and first-year students. She
-                  knows the importance of STEM and plans to bring coding to her
-                  former high school and middle school classrooms. She is also a
-                  current mentor in the PREP mentor program and a Co-Facilitator
-                  for the De Colores Club on campus.
+                Hello, I'm Marcos Hernandez, a second year student majoring in 
+                Electrical Engineering and Computer Sciences from Salinas, California. 
+                I am interested in pursuing a career in Software Engineering and meeting 
+                new people with similar interests, not just professional ones.
                 </Card.Description>
               </Card.Content>
               <Card.Content extra>
                 <Icon name="mail outline" bordered />
-                karenruizroca [at] berkeley [dot] edu{" "}
+                marcosh [at] berkeley [dot] edu{" "}
               </Card.Content>
             </Card>
           </Grid.Column>
@@ -155,25 +133,17 @@ function About() {
 
           <Grid.Column width={5}>
             <Card centered fluid>
-              <Image src={YoussefBio} wrapped ui={false} />
+              <Image src={LailaBio} wrapped ui={false} />
               <Card.Content>
-                <Card.Header>Youssef Salib</Card.Header>
+                <Card.Header>Laila Walker</Card.Header>
                 <Card.Meta>PREP CS Curriculum Co-Writer</Card.Meta>
                 <Card.Description>
-                  Youssef is a second-year student at UC Berkeley majoring in
-                  Electrical Engineering and Computer Sciences. He enjoys giving
-                  back to his community, and chose to return to the very same
-                  program that he participated in, as a tutor and mentor. This
-                  Pre-Engineering Progam (PREP) allowed Youssef to develop and
-                  deliver understandable, rigorous material to prepare students
-                  for Berkeley computer science.Youssef is actively looking for
-                  opportunities in industry and research to valuably contribute
-                  to important causes and initiatives.
+                 
                 </Card.Description>
               </Card.Content>
               <Card.Content extra>
                 <Icon name="mail outline" bordered />
-                youssef [dot] salib01 [at] berkeley [dot] edu
+                lailawalker[at] berkeley [dot] edu
               </Card.Content>
             </Card>
           </Grid.Column>
