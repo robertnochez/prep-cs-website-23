32493963ba2e40c86c316f9a078cca4a23583f23
Vincent Hong Lee <vlee747@berkeley.edu>
2022-07-27 22:12:48 -0700

    updated day 3 slides

--- src/pages/PREP/PREPLesson3.js
+++ src/pages/PREP/PREPLesson3.js
@@ -5,15 +5,14 @@ import PREPLesson from "./Lesson";
 function PREPLesson3() {
     return (
         <PREPLesson 
-        header="Functions and If-Statements"
+        header="Operators and Functions"
         description="Today's lesson will include how assignments are made. It will also focus on functions: their
-        anatomy, how to write them, and when they are necessary. We will also introduce if-statements: their 
-        structure and examples. "
-        slideSrc={`https://docs.google.com/presentation/d/e/2PACX-1vQz89eLN_PxZd94zgRAeotYg3dFyob3dFV1NYmP2jzG9hqKJ0b871Y4mpXMjutIeXLZSVSRM5GlZgbT/embed?start=false&loop=false&delayms=3000`}
+        anatomy, how to write them, and when they are necessary."
+        slideSrc={`https://docs.google.com/presentation/d/1Y4DH-pf3h980B4mX3s5aRVDC6mKous-PfXryOGW9dr8/embed?start=false&loop=false&delayms=3000`}
         prevLesson=""
         nextLesson=""
-        videoSrc={'https://www.youtube.com/embed/GbNe2si14sg'}
-        slideLink={`https://docs.google.com/presentation/d/1fPmNOmagso5gyqxniew7uQE5onsu3bbUblHCHN-WbvM/edit?usp=sharing`}
+        videoSrc={''}
+        slideLink={`https://docs.google.com/presentation/d/1Y4DH-pf3h980B4mX3s5aRVDC6mKous-PfXryOGW9dr8/edit?usp=sharing`}
         />
     )
 }
