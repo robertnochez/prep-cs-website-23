c5ca5b41a023c6cb6dc20a208324d112dfaa4ab8
Dest UL <destul@Destinys-MacBook-Pro.local>
2022-07-29 00:06:26 -0700

    Added Lesson 4

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,14 +8,22 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeItem: "3",
+      activeItem: "1",
       activePage: [
         <div>
           <Header as={"h3"}>
-            <b>Practice Problem Solutions</b>
+            <b>Welcome In!</b>
           </Header>
           <p>
-            Coming soon :)
+            We're happy to have you here, and look forward to helping you be
+            successful! We're happy to have you here, and look forward to helping
+            you be successful! Feel free to explore the website and the available
+            materials. We will release materials at 6pm the day before they are
+            going to be taught.
+          </p>
+          <p>
+            Future announcements will be posted here, so be sure to check back
+            regularly!
           </p>
         </div>,
       ],
@@ -89,7 +97,7 @@ class Carousel extends React.Component {
         <p>
           Moving forward, solutions to practice problems will be posted on their
           respective page the night of the lesson. For example, the solutions to
-          Lesson 7 practice problems are posted the evening of August 2nd.
+          Lesson 4 practice problems are posted the evening of July 29.
         </p>
         <p>
           While we may get to cover some problems during the lessons, we cannot
@@ -101,13 +109,49 @@ class Carousel extends React.Component {
         </p>
         <p>You can find the links to OH on the slide 2 of this section!!</p>
         <p>
-          <Link to="/prepcs-lesson6">Lesson 6 Solutions</Link>
+          <Link to="/prepcs-lesson6">Lesson 4- Solutions</Link>
         </p>
         <p>
           <Link to="/prepcs-lesson7">Lesson 7 Solutions</Link>
         </p>
         <p>
           <Link to="/prepcs-lesson9">Lesson 9 Solutions</Link>
+        </p>
+      </div>,
+    ];
+
+    const page4 = [
+      <div>
+        <Header as={"h3"}>
+          <b>Practice Problem Solutions</b>
+        </Header>
+        <p>
+          Moving forward, solutions to practice problems will be posted on their
+          respective page the night of the lesson. For example, the solutions to
+          Lesson 4 practice problems are posted the evening of July 29.
+        </p>
+        <p>
+          While we may get to cover some problems during the lessons, we cannot
+          guarantee that we will always have time. So, if you have questions or
+          would like to work out the problems with your peers or the
+          instructors, please drop by Office Hours. We are more than happy to
+          help you figure out the problems and answer any questions you may have
+          :)
+        </p>
+        <p>You can find the links to OH on the slide 2 of this section!!</p>
+        <p>
+          Solutions to Lesson 4 HW Problems are coming soon
+        </p>
+        <p>
+          <Link to="https://pythontutor.com/render.html#code=x%20%3D%203%0Ay%20%3D%202%0Az%20%3D%20'six'%0A%0Adef%20divideTwo%28num1,%20num2%29%3A%0A%20%20%20%20return%20num2//%20num1%0A%0Az%20%3D%20x%20*%20y%0AfReturn%20%3D%20divideTwo%28x,%20z%29&cumulative=false&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false">
+            Lesson 4 In Class: Example 1</Link>
+        </p>
+        <p>
+          <Link to="https://pythontutor.com/render.html#code=def%20cubeNum%28x%29%3A%0A%20%20%20%20return%20x%20*%20squareNum%28x%29%0A%20%20%20%20%0Adef%20squareNum%28y%29%3A%0A%20%20%20%20return%20y%20*%20oneTimes%28y%29%0A%20%20%20%20%0Adef%20oneTimes%28z%29%3A%0A%20%20%20%20return%201%20*%20z%0A%20%20%20%20%0AcubeNum%284%29%3B&cumulative=false&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false">
+            Lesson 4 In Class: Example 2</Link>
+        </p>
+        <p>
+          <Link to="/prepcs-lesson9">Lesson 4 In Class: Example 3</Link>
         </p>
       </div>,
     ];
--- src/pages/PREP/PREPLesson4.js
+++ src/pages/PREP/PREPLesson4.js
@@ -8,14 +8,22 @@ function PREPLesson4() {
           header="Control"
           description="This lesson expands on if-statements by adding on elif and else. After the examples of the if-elif-else 
           control block we will seek help from environment diagrams. "
-          slideSrc={`https://docs.google.com/presentation/d/e/2PACX-1vQvchYex4y1esn7nbRjOPzfH2NdtvoPEvNqjEr74cDGj5ajb5lomUR4SML-JYVc4E4mMt__w99bsNGR/embed?start=false&loop=false&delayms=3000`}
+          slideSrc={`https://docs.google.com/presentation/d/1tYj55paUcDWMzwKnX1XXUodm_fFnL9AmHrTTpK7MgMk/edit?usp=sharing`}
           prevLesson={null}
           nextLesson=""
-          videoSrc={`https://www.youtube.com/embed/ymTeLrLzsIM`}
+          videoSrc={``}
           lessonDownload="./prepDownloads/prep_lesson4.zip"
-          slideLink={`https://docs.google.com/presentation/d/1t6F-lPGgsKP5yCIXonO_3z3uwd0-dN_iwfoDMZPyy9Y/edit?usp=sharing`}
-        />
-    )
+          slideLink={`https://docs.google.com/presentation/d/1tYj55paUcDWMzwKnX1XXUodm_fFnL9AmHrTTpK7MgMk/edit?usp=sharing`}
+         > 
+        <p>Here are links to the Python Tutor solutiions for the in class examples:</p>
+        <Link to="https://pythontutor.com/render.html#code=x%20%3D%203%0Ay%20%3D%202%0Az%20%3D%20'six'%0A%0Adef%20divideTwo%28num1,%20num2%29%3A%0A%20%20%20%20return%20num2//%20num1%0A%0Az%20%3D%20x%20*%20y%0AfReturn%20%3D%20divideTwo%28x,%20z%29&cumulative=false&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false">
+          Lesson 4 In Class: Example 1</Link>
+        <Link to="https://pythontutor.com/render.html#code=def%20cubeNum%28x%29%3A%0A%20%20%20%20return%20x%20*%20squareNum%28x%29%0A%20%20%20%20%0Adef%20squareNum%28y%29%3A%0A%20%20%20%20return%20y%20*%20oneTimes%28y%29%0A%20%20%20%20%0Adef%20oneTimes%28z%29%3A%0A%20%20%20%20return%201%20*%20z%0A%20%20%20%20%0AcubeNum%284%29%3B&cumulative=false&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false">
+          Lesson 4 In Class: Example 2</Link>
+        <Link to="/prepcs-lesson9">Lesson 4 In Class: Example 3</Link>
+  </PREPLesson>
+  )
+        
 }
 
 export default PREPLesson4;
