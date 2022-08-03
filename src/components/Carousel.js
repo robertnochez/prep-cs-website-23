769a230506f07431250bb5a6d726e7a4503383b7
Dest UL <destul@Destinys-MacBook-Pro.local>
2022-08-03 00:05:08 -0700

    Fixed Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -32,10 +32,10 @@ class Carousel extends React.Component {
           Solutions to Lesson 4 HW Problems are on Lesson 4 - Controls!
         </p>
         <p>
-        <Link href="/cs-central/prepcs-lesson4">Lesson 4 Solutions</Link>
+        <Link to="/prepcs-lesson4">Lesson 4 Solutions</Link>
         </p>
         <p>
-        <Link href="/cs-central/prepcs-lesson6">Lesson 6 Solutions</Link>
+        <Link to="/prepcs-lesson6">Lesson 6 Solutions</Link>
         </p>
         </div>,
       ],
@@ -126,7 +126,7 @@ class Carousel extends React.Component {
 
     const page4 = [
       <div>
-        <Header as={"h3"}>
+          <Header as={"h3"}>
           <b>Practice Problem Solutions</b>
         </Header>
         <p>
@@ -147,18 +147,12 @@ class Carousel extends React.Component {
           Solutions to Lesson 4 HW Problems are on Lesson 4 - Controls!
         </p>
         <p>
-          <a href="https://pythontutor.com/render.html#code=x%20%3D%203%0Ay%20%3D%202%0Az%20%3D%20'six'%0A%0Adef%20divideTwo%28num1,%20num2%29%3A%0A%20%20%20%20return%20num2//%20num1%0A%0Az%20%3D%20x%20*%20y%0AfReturn%20%3D%20divideTwo%28x,%20z%29&cumulative=false&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false">
-            Lesson 4 In Class: Example 1</a>
+        <Link to="/prepcs-lesson4">Lesson 4 Solutions</Link>
         </p>
         <p>
-          <a href="https://pythontutor.com/render.html#code=def%20cubeNum%28x%29%3A%0A%20%20%20%20return%20x%20*%20squareNum%28x%29%0A%20%20%20%20%0Adef%20squareNum%28y%29%3A%0A%20%20%20%20return%20y%20*%20oneTimes%28y%29%0A%20%20%20%20%0Adef%20oneTimes%28z%29%3A%0A%20%20%20%20return%201%20*%20z%0A%20%20%20%20%0AcubeNum%284%29%3B&cumulative=false&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false">
-            Lesson 4 In Class: Example 2</a>
+        <Link to="/prepcs-lesson6">Lesson 6 Solutions</Link>
         </p>
-        <p>
-          <a href="https://pythontutor.com/render.html#code=gem,%20stone%20%3D%20'sapphires',%20'diamonds'%0AdiaTotal,%20sapTotal,%20rubTotal%20%3D%2045,%2035,%200%0A%0Adef%20addStudents%28blue,%20white,%20they,%20them%29%3A%0A%20%20%20%20max%20%3D%2099%0A%20%20%20%20all%20%3D%20they%20%2B%20them%0A%20%20%20%20if%20all%20%3E%2099%3A%0A%20%20%20%20%20%20%20%20gemstones%20%3D%20'too%20many!'%0A%20%20%20%20elif%20all%20%3C%20101%3A%0A%20%20%20%20%20%20%20%20gemstones%20%3D%20addNames%28blue,%20white%29%0A%20%20%20%20print%28gemstones%29%0A%20%20%20%20return%20moreGems%28%29%0A%20%20%20%20%0Adef%20addNames%28oh,%20no%29%3A%0A%20%20%20%20return%20oh%20%2B%20'%20%26%20'%20%2B%20no%20%0A%20%20%20%20%0Adef%20moreGems%28%29%3A%0A%20%20%20%20global%20rubTotal%0A%20%20%20%20jewel,%20treasure%20%20%3D%20'rubies',%20'emeralds'%0A%20%20%20%20rubTotal,%20emeTotal%20%3D%2012,%2090%0A%20%20%20%20more%20%3D%20rubTotal%20%2B%20emeTotal%0A%20%20%20%20print%28more%29%0A%20%20%20%20return%20gem%20%2B%20'%20%26%20'%20%2B%20stone%20%2B%20'%20%26%20'%20%2B%20jewel%20%2B%20'%20%26%20'%20%2B%20treasure%0A%20%20%20%20%0A%20%20%20%20%0AaddStudents%28gem,%20stone,%20diaTotal,%20sapTotal%29&cumulative=false&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false">
-            Lesson 4 In Class: Example 3</a>
-        </p>
-      </div>,
+        </div>,
     ];
 
     return (
