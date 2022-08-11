bec340dcddf5bb5754b3aedb6774e4c461361e7b
Dest UL <destul@Destinys-MacBook-Pro.local>
2022-08-11 01:12:10 -0700

    Lesson13 Added

--- src/pages/PREP/PREPLesson13.js
+++ src/pages/PREP/PREPLesson13.js
@@ -5,13 +5,13 @@ import PREPLesson from "./Lesson";
 function PREPLesson13() {
     return (
         <PREPLesson
-          header="Recursion Practice"
-          description="We're done with content!! Now on to review. This lesson is a summative review of what we have covered so far."
-          slideSrc={`https://docs.google.com/presentation/d/e/2PACX-1vTFA1vZi8v-BUDXx-0-d8g1WV-25pOGiTi1LnwsSWZ7hP0yLluguMdxvUk1ProiIltcD39zXIwURZMA/embed?start=false&loop=false&delayms=3000`}
+          header="Exam Practice and Wrap Up"
+          description="We're done with content!! Thanks for joining us on this journey! Today we'll be taking a look at some exam practice questions."
+          slideSrc={`https://docs.google.com/presentation/d/1J2BTdMQjRTrHDPuoNFEsaUAx7eUvrbLxlXdD-_AO2x8/embed?start=false&loop=false&delayms=3000`}
           prevLesson={null}
           nextLesson=""
-          slideLink="https://docs.google.com/presentation/d/1a0RXKnM9Xq7PXPThIQ3cQjmiQ--SX5s0QgV9JzP0Y84/edit?usp=sharing"
-          videoSrc={`https://www.youtube.com/embed/zfP4OuO-844`}
+          slideLink="https://docs.google.com/presentation/d/1J2BTdMQjRTrHDPuoNFEsaUAx7eUvrbLxlXdD-_AO2x8/edit?usp=sharing"
+          videoSrc={``}
         />
     )
 }
