6e719158eb28a7d116a46b3dced10287175032ad
destinyluong <destinyul2010@gmail.com>
2023-07-11 22:08:43 -0700

    Revised Lesson 2

--- src/pages/PREP/PREPLesson2.js
+++ src/pages/PREP/PREPLesson2.js
@@ -5,17 +5,17 @@ import PREPLesson from "./Lesson";
 function PREPLesson2() {
   return (
     <PREPLesson
-      header="Python Syntax"
+      header="Datatypes and Operands"
       description="An introduction to Python, including data types and its basic syntax. Definitions for 
       different python operators and how to evaluate expressions using orders of operations, analogies, and examples. Keep 
       in mind that the actual language we're using doesn't matter too much, since what we're trying to teach here are features
       that are common to every programming language. This is a skill that will be useful for both CS10 and CS61A. :o"
-      slideSrc={`https://docs.google.com/presentation/d/1y9emWtx2hpJLg0mB_mXQ_pvegJZ2ulsMi7ChuyGt9NQ/embed?start=false&loop=false&delayms=3000`}
+      slideSrc={`https://docs.google.com/presentation/d/1qOd7l9czGt4tX2Y6Zl6DIbliKS4anbCOyUc8itPMuZA/embed?start=false&loop=false&delayms=3000`}
       prevLesson=""
       nextLesson=""
       released={true}
       videoSrc=""
-      slideLink={`https://docs.google.com/presentation/d/1y9emWtx2hpJLg0mB_mXQ_pvegJZ2ulsMi7ChuyGt9NQ/edit?usp=sharing`}
+      slideLink={`https://docs.google.com/presentation/d/1qOd7l9czGt4tX2Y6Zl6DIbliKS4anbCOyUc8itPMuZA/edit?usp=sharing`}
     />
   );
 }
