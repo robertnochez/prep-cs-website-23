642093096d869a1cf0d74fa897949b88ec480ee8
destinyluong <destinyul2010@gmail.com>
2023-07-23 21:00:42 -0700

    Fixed Lesson Info

--- src/pages/PREP/PREPLesson8.js
+++ src/pages/PREP/PREPLesson8.js
@@ -7,16 +7,131 @@ import { Header } from "semantic-ui-react";
 function PREPLesson8() {
   return (
     <PREPLesson
-      header="Linked Lists"
-      description="An brief introduction to classes, object-oriented programming, and linked lists."
-          slideSrc={`https://docs.google.com/presentation/d/1r1rrPoxyWcSBMTdq337c7dg6M2CoJfmueWqLSjW4g4M/embed?start=false&loop=false&delayms=3000`}
+      header="More HOFs, Iterators and Generators"
+      description="Going over HOFs, using iterators and creating generators"
+          slideSrc={`https://docs.google.com/presentation/d/12e9Ism1VJYhliNYstddQzlpWufG34mcCR_n-L3q79XM/embed?start=false&loop=false&delayms=3000`}
           prevLesson={null}
           nextLesson=""
           videoSrc={``}
-          slideLink={`https://docs.google.com/presentation/d/1r1rrPoxyWcSBMTdq337c7dg6M2CoJfmueWqLSjW4g4M/edit?usp=sharing`}
-          lessonDownload="./prepDownloads/prep_lesson8.zip"
+          slideLink={`https://docs.google.com/presentation/d/12e9Ism1VJYhliNYstddQzlpWufG34mcCR_n-L3q79XM/edit?usp=sharing`}
+          lessonDownload="./prepDownloads/prep_lesson7.zip"
         >
       <Header as={"h3"}>Practice Problem Solutions:</Header>
+      <CodeBlock
+          code={`
+          
+######################################################
+######### iter.py PRACTICE PROBLEM SOLUTIONS #########
+######################################################
+
+def pairRAs(RA_List):
+    """ We've lost track of which RAs are supposed to be teaching together! Please list all the
+    possible pairs of RAs for each lesson using a loop. Remember there can only be 2 RAs per lesson, 
+    they cannot pair with themselves, and the pairs cannot repeat.
+ 
+    >>> pairRAs(RAs)
+    Laila and Marcos
+    Laila and Destiny
+    Laila and Vincent
+    Marcos and Destiny
+    Marcos and Vincent
+    Destiny and Vincent
+    
+    """
+    pass
+    i = 0
+    for RAOne in RA_List:
+        for RATwo in RA_List[i+1:]:
+            if RAOne != RATwo:
+                print(RAOne + ' and ' + RATwo)
+        i += 1
+
+ 
+def createRectangle(num, x, y):
+    """ Create a picture of a rectangle out of the given number depending
+    on the the dimensions given using a loop. Hint: how
+ 
+    >>> createRectangle(3, 6, 5)
+    333333
+    333333
+    333333
+    333333
+    333333
+
+
+    >>> createRectangle(1, 4, 2)
+    1111
+    1111
+
+    """
+    pass
+    
+    row = 0
+    while row < y:
+        col = 0
+        numStr = ""
+        while col < x:
+            numStr += str(num)
+            if col == x-1:
+                print(numStr)
+            col += 1  
+        row += 1 
+
+#######################################################
+######### loops.py PRACTICE PROBLEM SOLUTIONS #########
+#######################################################
+
+def groceryList(itemLst):
+    """Given the list 'grocery' print each item on the list one by one
+    using an iterator!
+ 
+    >>> groceryList(shoppingLst1)
+    cabbage
+    olive oil
+    milk
+    tomatoes
+    hand soap
+    >>> groceryList(shoppingLst2)
+    yogurt
+    frozen pizza
+    spinach
+    orange juice
+    sour cream
+    """
+
+    iter_list = iter(itemLst)
+    i = 0 
+    while i < len(itemLst):
+        item = next(iter_list)
+        print(item)
+        i += 1
+    
+
+
+ 
+def trickOrTreat(houses):
+    """ You are going trick or treating! Create a generator that check whether each
+    house has a trick or a treat by yielding "[Insert address here] has given you a trick!"
+    or "[Insert address here] has given you a treat!"
+ 
+    >>> TorTgen = trickOrTreat(houseLst)
+    >>> next(TorTgen)
+    '309 Witches Ave has given you a trick!'
+    >>> next(TorTgen)
+    '470 Fairy Way has given you a treat!'
+    >>> next(TorTgen)
+    '906 Batman Cave has given you a treat!'
+    """
+    for i in range(len(houses)):
+      yield list(houses.keys())[i] + " has given you a " + list(houses.values())[i] + "!"        
+
+`}
+        />
+        </PREPLesson>
+    )
+}
+
+/*
 
 <CodeBlock
   code={`
@@ -124,9 +239,6 @@ def reverseLst(linkedList):
 
 
 `} 
-/>
-        </PREPLesson>
-    )
-}
+/>*/ 
 
 export default PREPLesson8;
--- src/pages/PREP/PREPLesson9.js
+++ src/pages/PREP/PREPLesson9.js
@@ -8,15 +8,78 @@ import { Header } from "semantic-ui-react";
 function PREPLesson9() {
   return (
     <PREPLesson
-      header="Recursion"
+      header="Lambdas and Classes"
       description=""
-      slideSrc={`https://docs.google.com/presentation/d/1j6i3twWef3Mb6fxLHK49cQKUx6t5d3joldNiTO42tJI/embed?start=false&loop=false&delayms=3000`}
+      slideSrc={`https://docs.google.com/presentation/d/1wGiAR2Yfib-BBFBVXqkLc1kDAJA3ZL1rcW5fIF8p_Ts/embed?start=false&loop=false&delayms=3000`}
       prevLesson={null}
       nextLesson=""
-      slideLink="https://docs.google.com/presentation/d/1j6i3twWef3Mb6fxLHK49cQKUx6t5d3joldNiTO42tJI/edit?usp=sharing"
-      lessonDownload="./prepDownloads/Spotify.py"
+      slideLink="https://docs.google.com/presentation/d/1wGiAR2Yfib-BBFBVXqkLc1kDAJA3ZL1rcW5fIF8p_Ts/edit?usp=sharing"
+      lessonDownload="./prepDownloads/prep_lesson10.zip"
       videoSrc={``}
     >
+     <Header as={"h3"}>Practice Problem Solutions:</Header>
+    <CodeBlock
+          code={`
+  """
+  NOTE: THERE WERE ERRORS IN THE DOCTESTS. IF YOUR CODE IS OUTPUTTING THE RIGHT THING
+  THEN IT IS MOST LIKELY RIGHT. CHECK DOWN BELOW FOR THE ERRORS. THERE ARE COMMENTS
+  IN THE DOCSTRINGS WHERE THE ERRORS WERE.
+  """
+  fn = lambda x: lambda y: pow(x, y)
+  def map_recursive(start, end, f):
+      """Implement map_to_range from Tuesday's lesson recursively
+      
+      >>> map_recursive(0, 10, fn(2))         ## there was accidentally two 128's in doctest
+      1
+      2
+      4
+      8
+      16
+      32
+      64
+      128        
+      256
+      512
+      """
+      if start == end:
+          return
+      print(f(start))
+      return map_recursive(start + 1, end, f)
+  def fib(i):
+      """Return the i-th term of the Fibonacci Sequence.
+      
+      >>> fib(2)    # --> 0 + 1
+      1
+      >>> fib(5)    # --> 2 + 3
+      5
+      >>> fib(10)   # --> 21 + 34
+      55
+      """
+      if i == 0:
+          return 0
+      elif i == 1:
+          return 1
+      else:
+          return fib(i - 1) + fib(i - 2)
+  def factorial_hof(n):
+      """Now that we have defined factorial using iteration and recursion,
+      implement it using a higher order function. You are not allowed to use
+      operators outside of the function calls. 
+      --> Think about what we can do to store a counter
+      >>> factorial_hof(5)
+      120
+      >>> factorial_hof(6) ## error in this doctect
+      720
+      """
+      def helper(x, y):
+          if x == 0:
+              return y
+          else:
+              return helper(x - 1, y * x)
+      return helper(n, 1)
+  `} 
+  />
+
     </PREPLesson>
   );
 }
