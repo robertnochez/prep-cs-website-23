6f6d0c34192a763cf6debc5a4ff0935a4707efd9
destinyluong <destinyul2010@gmail.com>
2023-08-06 14:41:37 -0700

    Changed title

--- src/pages/PREP/PREPLesson13.js
+++ src/pages/PREP/PREPLesson13.js
@@ -5,8 +5,8 @@ import PREPLesson from "./Lesson";
 function PREPLesson13() {
     return (
         <PREPLesson
-          header="Exam Practice and Wrap Up"
-          description="We're done with content!! Thanks for joining us on this journey! Today we'll be taking a look at some exam practice questions."
+          header="Jeopardy"
+          description="We're done with content!! Thanks for joining us on this journey! Today we'll be taking playing practice jeopardy!"
           slideSrc={`https://docs.google.com/presentation/d/1LgzFL2yMRplmfZypzc07yfxZrSVfbTBE8DoJkveMTCg/embed?start=false&loop=false&delayms=3000`}
           prevLesson={null}
           nextLesson=""
